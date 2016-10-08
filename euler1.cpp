3df9cad66cd5d716d8193a8003224a1bd7419b94
Christian Philip Cayanan <cayananchristian45@gmail.com>
2016-10-08 11:50:32 +0800

    Create euler1.cpp

---
+++ euler1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+#include<fstream>
+using namespace std;
+
+int main(){
+	ifstream f("input.txt");
+	int num;
+	
+	while (f>>num){
+	      int result = 0;
+     for (int j=0; j<num; j++){
+          if((j%3==0)||(j%5==0)){result+=j;}
+     }
+     cout<<result<<endl;
+	}
+	system ("pause > 0");
+}
